9fa26ec6d2c7805cd09059c1f7e931e125551610
adrianmarikar <adrianmarikar@gmail.com>
2023-10-03 19:26:06 +0100

    Pushing files after clearing cache.

---
+++ MysticMesh/src/MysticMesh.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by MysticMesh applications
+
+#include "MysticMesh/Application.h"
+
+// ----------Entry Point---------------------
+#include "MysticMesh/EntryPoint.h"
+// ------------------------------------------
